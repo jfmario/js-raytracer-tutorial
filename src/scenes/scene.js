ab82b94bcc2918e96737a806a90cb7fa9eabdf81
EC2 Default User <ec2-user@ip-172-31-12-177.us-west-2.compute.internal>
2018-05-20 00:00:16 +0000

    generate random spheres

--- src/scenes/scene.js
+++ src/scenes/scene.js
@@ -1,13 +1,35 @@
 
+import Sphere from '../geometry/sphere';
+import Vector3 from '../data-structures/vector3';
+
 class Scene {
+  
   constructor(xmin=-2, xmax=2, ymin=-1, ymax=-1, zmin=0, zmax=5) {
+    
     this.xmin = xmin;
     this.xmax = xmax;
     this.ymin = ymin;
     this.ymax = ymax;
     this.zmin = zmin;
     this.zmax = zmax;
+    this.objects = [];
+  }
+  
+  generateRandomSpheres(amount=5) {
+    for (var i = 0; i < amount; ++i) {
+      
+      let center = new Vector3(
+        Math.random() * (this.xmax - this.xmin) + this.xmin,
+        Math.random() * (this.ymax - this.ymin) + this.ymin,
+        Math.random() * (this.zmax - this.zmin) + this.zmin
+      );
+      let radius = Math.random() * (1 - 0.1) + 0.1;
+      
+      let sphere = new Sphere(center, radius);
+      this.objects.push(sphere);
+    }
   }
 }
 
+export default Scene;
 export default Scene;
